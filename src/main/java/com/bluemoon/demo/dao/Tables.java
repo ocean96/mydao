6b9d9d801ceca5baa7e8595e7622a31708c61e99
ocean <oceanjava6@gmail.com>
2016-08-08 22:28:31 +0800

    no message

--- src/main/java/com/bluemoon/demo/dao/Tables.java
+++
@@ -1,7 +0,0 @@
-package com.bluemoon.demo.dao;
-
-import com.bluemoon.demo.Record.UserRecord;
-
-public class Tables {
-	public static final UserRecord USER = com.bluemoon.demo.Record.UserRecord.USER;
-}
